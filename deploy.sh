f1a75a7968e23ca5ada863332b2e9a967498aa4a
EC2 Default User <ec2-user@ip-172-31-0-19.ap-south-1.compute.internal>
2025-10-29 16:30:40 +0000

    added hello.py and deploy.sh for cicd pipeline

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+echo "---CI/CD Pipeline Started---"
+
+echo "step1: Building the application"
+
+sleep 1
+
+echo "Step2: Running Tests..."
+sleep 1
+echo "Test passed Successfully"
+sleep 2
+
+echo "Step3: Deploying Application"
+sleep 1
+echo "Application Deployed Successfully..."
+echo "---CI/CD Pipeline Finished---"
+
